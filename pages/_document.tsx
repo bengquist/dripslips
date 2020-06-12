af29427b5ac48d24c55310fc92b12fc85bd57d23
bengquist <engquist_blake@yahoo.com>
2020-06-12 00:04:30 -0500

    initial

---
+++ pages/_document.tsx
@@ -0,0 +1,34 @@
+import { Request } from "express";
+import { NextContext } from "next";
+import { AppProps } from "next/app";
+import Document from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: NextContext<Request>) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props: AppProps) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+}
