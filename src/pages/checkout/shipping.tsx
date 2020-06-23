233a1962ea2c9bcd4e5b03e9b371fb3fe25c9795
bengquist <engquist_blake@yahoo.com>
2020-06-23 16:46:02 -0500

    add layout for shipping page

--- src/pages/checkout/shipping.tsx
+++ src/pages/checkout/shipping.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+import { flexAlignCenter } from "../../style/helpers";
 
 const CheckoutShippingPage = () => {
-  return <div>shipping</div>;
+  return (
+    <Container>
+      <Section>yo</Section>
+      <Aside>aside</Aside>
+    </Container>
+  );
 };
 
 export default CheckoutShippingPage;
+
+const Container = styled.div`
+  ${flexAlignCenter}
+  height: 100%;
+`;
+
+const Section = styled.section`
+  height: 100%;
+  width: 70%;
+  padding: 3rem;
+  background: ${({ theme }) => theme.colors.shadedWhite};
+`;
+
+const Aside = styled.aside`
+  width: 30%;
+  height: 100%;
+`;
