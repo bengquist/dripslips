69f9e73ee4426a806761801e2c341af50b9c6f14
bengquist <engquist_blake@yahoo.com>
2020-07-15 16:40:39 -0500

    fix product route

--- src/routing/routes.ts
+++ src/routing/routes.ts
@@ -8,5 +8,5 @@ export default {
   MY_PROFILE: "/me",
   MY_ORDERS: "/me/orders",
   MY_WISHLIST: "/me/wishlist",
-  PRODUCT: (id: string) => ({ href: "/product/[id]", as: `/product/[${id}]` }),
+  PRODUCT: (id: string) => ({ href: "/product/[id]", as: `/product/${id}` }),
 };
